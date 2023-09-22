779cc89a587cef257297a58e6920c42770d41d67
harsh <hrmishra2000@gmail.com>
2023-09-22 16:53:25 +0530

    add commands as constant

---
+++ commands/command.go
@@ -0,0 +1,9 @@
+package commands
+
+const (
+	CreateAccount      = "create_account"
+	DepositMoney       = "deposit"
+	WithdrawMoney      = "withdraw"
+	DisplayBalance     = "display_balance"
+	DisplayBankBalance = "display_bank_balance"
+)
